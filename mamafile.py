c88f41fb929cc4b644a8573dd2ce74f3d0dee48e
Jorma <jorma.rebane@gmail.com>
2018-08-04 02:21:23 +0300

    Updated mamafile

--- mamafile.py
+++ mamafile.py
@@ -8,18 +8,18 @@ class opencv(mama.BuildTarget):
     def configure(self):
         opt = [
             "ENABLE_OMIT_FRAME_POINTER=ON", "ENABLE_PRECOMPILED_HEADERS=ON", "ENABLE_CCACHE=ON",
-            "BUILD_DOCS=OFF",  "BUILD_EXAMPLES=OFF", "BUILD_TESTS=OFF", "BUILD_PERF_TESTS=OFF",
-            "WITH_OPENGL=ON",  "WITH_IPP=OFF",    "WITH_OPENCL=OFF", "WITH_1394=OFF",    "WITH_CUDA=OFF",
-            "WITH_OPENGL=ON",  "WITH_JASPER=OFF", "WITH_WEBP=OFF",   "WITH_OPENEXR=OFF", "WITH_TIFF=OFF", "WITH_FFMPEG=OFF",
+            "BUILD_DOCS=OFF", "BUILD_EXAMPLES=OFF", "BUILD_TESTS=OFF", "BUILD_PERF_TESTS=OFF",
+            "WITH_OPENGL=ON", "WITH_IPP=OFF",    "WITH_OPENCL=OFF", "WITH_1394=OFF",    "WITH_CUDA=OFF",
+            "WITH_OPENGL=ON", "WITH_JASPER=OFF", "WITH_WEBP=OFF",   "WITH_OPENEXR=OFF", "WITH_TIFF=OFF", "WITH_FFMPEG=OFF",
             "BUILD_OPENEXR=OFF", "BUILD_TIFF=OFF", "BUILD_JPEG=ON",
             "BUILD_PNG=ON",      "BUILD_ZLIB=ON",  "BUILD_JASPER=OFF",
             "BUILD_opencv_apps=OFF",      "BUILD_opencv_calib3d=ON",   "BUILD_opencv_core=ON",
             "BUILD_opencv_features2d=ON", "BUILD_opencv_flann=ON",     "BUILD_opencv_highgui=ON",
             "BUILD_opencv_imgcodecs=ON",  "BUILD_opencv_imgproc=ON",   "BUILD_opencv_ml=ON",
-            "BUILD_opencv_objdetect=ON",  "BUILD_opencv_photo=ON",    "BUILD_opencv_shape=OFF",
+            "BUILD_opencv_objdetect=ON",  "BUILD_opencv_photo=ON",     "BUILD_opencv_shape=OFF",
             "BUILD_opencv_stitching=OFF", "BUILD_opencv_superres=OFF", "BUILD_opencv_ts=OFF",
             "BUILD_opencv_video=ON",      "BUILD_opencv_videoio=ON",   "BUILD_opencv_videostab=OFF",
-            "BUILD_opencv_nonfree=OFF", "BUILD_SHARED_LIBS=OFF", "BUILD_opencv_java=OFF", 
+            "BUILD_opencv_nonfree=OFF", "BUILD_SHARED_LIBS=OFF",    "BUILD_opencv_java=OFF", 
             "BUILD_opencv_python2=OFF", "BUILD_opencv_python3=OFF", "BUILD_opencv_xphoto=ON"
         ]
         if   self.android: opt += ['BUILD_ANDROID_EXAMPLES=OFF', 'BUILD_opencv_androidcamera=ON']
@@ -31,7 +31,7 @@ class opencv(mama.BuildTarget):
         self.cmake_build_type = 'Release'
         self.cmake_ios_toolchain = '../platforms/ios/cmake/Toolchains/Toolchain-iPhoneOS_Xcode.cmake'
         if self.linux:
-            self.add_cxx_flags('-mfma')
+            self.add_cl_flags('-mfma')
         if self.ios:
             self.disable_ninja_build() # opencv for ios blows up with Ninja
 
