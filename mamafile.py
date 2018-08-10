85fd21a19ace89851e90d6e88f79f11bee674278
Jorma <jorma.rebane@gmail.com>
2018-08-10 14:49:14 +0300

    Removed LAPACK

--- mamafile.py
+++ mamafile.py
@@ -9,8 +9,9 @@ class opencv(mama.BuildTarget):
         opt = [
             "ENABLE_OMIT_FRAME_POINTER=ON", "ENABLE_PRECOMPILED_HEADERS=ON", "ENABLE_CCACHE=ON",
             "BUILD_DOCS=OFF", "BUILD_EXAMPLES=OFF", "BUILD_TESTS=OFF", "BUILD_PERF_TESTS=OFF",
-            "WITH_OPENGL=ON", "WITH_IPP=OFF",    "WITH_OPENCL=OFF", "WITH_1394=OFF",    "WITH_CUDA=OFF",
-            "WITH_OPENGL=ON", "WITH_JASPER=OFF", "WITH_WEBP=OFF",   "WITH_OPENEXR=OFF", "WITH_TIFF=OFF", "WITH_FFMPEG=OFF",
+            "WITH_OPENGL=ON",   "WITH_IPP=OFF",    "WITH_OPENCL=OFF", "WITH_1394=OFF",
+            "WITH_CUDA=OFF",    "WITH_OPENGL=ON",  "WITH_JASPER=OFF", "WITH_WEBP=OFF",
+            "WITH_OPENEXR=OFF", "WITH_TIFF=OFF",   "WITH_FFMPEG=OFF", "WITH_LAPACK=OFF",
             "BUILD_OPENEXR=OFF", "BUILD_TIFF=OFF", "BUILD_JPEG=ON",
             "BUILD_PNG=ON",      "BUILD_ZLIB=ON",  "BUILD_JASPER=OFF",
             "BUILD_opencv_apps=OFF",      "BUILD_opencv_calib3d=ON",   "BUILD_opencv_core=ON",
@@ -19,8 +20,8 @@ class opencv(mama.BuildTarget):
             "BUILD_opencv_objdetect=ON",  "BUILD_opencv_photo=ON",     "BUILD_opencv_shape=OFF",
             "BUILD_opencv_stitching=OFF", "BUILD_opencv_superres=OFF", "BUILD_opencv_ts=OFF",
             "BUILD_opencv_video=ON",      "BUILD_opencv_videoio=ON",   "BUILD_opencv_videostab=OFF",
-            "BUILD_opencv_nonfree=OFF", "BUILD_SHARED_LIBS=OFF",    "BUILD_opencv_java=OFF", 
-            "BUILD_opencv_python2=OFF", "BUILD_opencv_python3=OFF", "BUILD_opencv_xphoto=ON"
+            "BUILD_opencv_nonfree=OFF",   "BUILD_SHARED_LIBS=OFF",     "BUILD_opencv_java=OFF", 
+            "BUILD_opencv_python2=OFF",   "BUILD_opencv_python3=OFF",  "BUILD_opencv_xphoto=ON"
         ]
         if   self.android: opt += ['BUILD_ANDROID_EXAMPLES=OFF', 'BUILD_opencv_androidcamera=ON']
         elif self.ios:     opt += ['IOS_ARCH=arm64']
